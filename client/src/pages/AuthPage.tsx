56873069c971568b02094b9ad23d4f37a6ee4259
andrewenletoh <105510337+andrewenletoh@users.noreply.github.com>
2026-09-16 12:13:46 -0700

    commenting out some code  for safety

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -48,10 +48,10 @@ const AuthPage = () => {
         }
     };
 
-    const switchMode = () => {
-        setMode((current) => (current === 'login' ? 'register' : 'login'));
-        reset();
-    };
+    // const switchMode = () => {
+    //     setMode((current) => (current === 'login' ? 'register' : 'login'));
+    //     reset();
+    // };
 
     return (
         <div className={styles.page}>
